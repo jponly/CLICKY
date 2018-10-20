9cba38647f5d400d7397fff2b0e8b93206159c3c
jponly <jpangeli514@gmail.com>
2018-10-20 10:04:33 -0700

    versionD

--- src/App.js
+++ src/App.js
@@ -29,6 +29,14 @@ class App extends Component {
     // push new array into state
     this.setState({ picked: tempArray });
     
+let copyArray = [...friends];
+
+for (let i = copyArray.length -1; i > 0; i-- ) {
+  const j = Math.floor(Math.random() * (i + 1));
+  [copyArray[i], copyArray[j]] = [copyArray[j], copyArray[i]];
+
+}
+ this.setState({ friends: copyArray });
   };
 
   
