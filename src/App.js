71d47d6255e182f4430744767711481e09dc28ab
jponly <jpangeli514@gmail.com>
2018-10-20 15:59:28 -0700

    versionO

--- src/App.js
+++ src/App.js
@@ -2,57 +2,65 @@ import React, { Component } from "react";
 import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
+import Navbar from "./components/Navbar";
 import friends from "./friends.json";
 import "./App.css";
 
 class App extends Component {
-  
+
   state = {
     friends,
-    picked: []
+    picked: [],
+    score: 0
+
   };
 
   chooseFriend = id => {
-    
+    // conditional for picks //
     let tempArray = this.state.picked;
-
-    // if this id in tempArrray : alert lose
-    // else add id to array
-    
     if (tempArray.indexOf(id) >= 0) {
       alert("You lose");
-  } else {
-     tempArray.push(id);
-  }
-  
-    
-    // push new array into state
+    } else {
+      tempArray.push(id);
+      alert("You won");
+
+      // score //
+      this.setState(prevState => {
+        return { score: prevState.score + 1 }
+      });
+    }
+
+    // shuffles cards //
     this.setState({ picked: tempArray });
-    
-let copyArray = [...friends];
+    let copyArray = [...friends];
+    for (let i = copyArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [copyArray[i], copyArray[j]] = [copyArray[j], copyArray[i]];
 
-for (let i = copyArray.length -1; i > 0; i-- ) {
-  const j = Math.floor(Math.random() * (i + 1));
-  [copyArray[i], copyArray[j]] = [copyArray[j], copyArray[i]];
-
-}
- this.setState({ friends: copyArray });
+    }
+    this.setState({ friends: copyArray });
   };
 
-  
+
+
   render() {
     return (
       <Wrapper>
-        <Title></Title>
+
+        <Navbar>{this.state.score}</Navbar>
+
+
+
+        <Title>Click Game</Title>
         {this.state.friends.map(friend => (
           <FriendCard
-          chooseFriend={this.chooseFriend}
-          id={friend.id}
-          key={friend.id}
-          name={friend.name}
-          image={friend.image}
-          occupation={friend.occupation}
-          location={friend.location} 
+            chooseFriend={this.chooseFriend}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+            occupation={friend.occupation}
+            location={friend.location}
           />
         ))}
       </Wrapper>
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Navbar.css";
+const styles = {
+  card: {
+    margin: 20,
+    background: "#e8eaf6"
+  },
+  heading: {
+    background: "#9a74db",
+    minHeight: 50,
+    lineHeight: 3.5,
+    fontSize: "1.2rem",
+    color: "white",
+    padding: "0 20px"
+  }
+};
+
+const Navbar = props => (
+  <div style={styles.card}>
+    <div style={styles.heading}>Score: {props.children}</div>
+  </div>
+);
+
+export default Navbar;
