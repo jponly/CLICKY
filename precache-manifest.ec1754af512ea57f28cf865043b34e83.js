c444a22ec2313bc58109c1177ad77a378dd3343d
jponly <jpangeli514@gmail.com>
2018-10-20 22:50:03 -0700

    Updates

---
+++ precache-manifest.ec1754af512ea57f28cf865043b34e83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "c8d6c15c1e93d697f8df",
+    "url": "/my-app/static/js/main.c8d6c15c.chunk.js"
+  },
+  {
+    "revision": "6fea6cd249d4f943684f",
+    "url": "/my-app/static/js/1.6fea6cd2.chunk.js"
+  },
+  {
+    "revision": "c8d6c15c1e93d697f8df",
+    "url": "/my-app/static/css/main.d697e0a3.chunk.css"
+  },
+  {
+    "revision": "218b5fbc9cdedb6d3f9e754ade37868b",
+    "url": "/my-app/index.html"
+  }
+];
